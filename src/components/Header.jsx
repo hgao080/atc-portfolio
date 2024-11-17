bc3a3a618f40acf791d9da601934c10efe67b28f
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-17 13:02:17 +1300

    Finished header visuals. No functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,17 @@
 const Header = () => {
     return ( 
-        <div className="">Jason</div>
+        <div className="flex relative border border-red-800 justify-center items-center h-28">
+            <div className="flex items-center absolute left-[40px] bg-[#7D7D7D22] px-[30px] h-[55px] rounded-full shadow-lg">
+                <img src="/atc-portfolio/assets/logo.png" alt="" className="w-[80px] mt-[4px] h-auto" />
+            </div>
+            <div className="flex items-center text-lg text-white font-header bg-[#7D7D7D22] px-[80px] h-[55px] rounded-full shadow-lg">
+                <ul className="flex gap-20">
+                    <li className="">Homepage</li>
+                    <li className="">ATC Projects</li>
+                    <li className="">Developers Section</li>
+                </ul>
+            </div>
+        </div>
      );
 }
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        background: "url('/public/assets/bg.png')",
+      },
+      fontFamily: {
+        header: ['Header'],
+      }
+    },
   },
   plugins: [],
 }
