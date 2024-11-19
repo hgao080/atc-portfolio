c0028e106fb1c7c71e7b2e0876d7bba28566ec12
sli683 <sli683@aucklanduni.ac.nz>
2024-11-19 17:09:57 +1300

    added project section with place holder elements

---
+++ src/components/Projects.jsx
@@ -0,0 +1,29 @@
+const Projects = () => {
+    return (
+        <div className="container mx-auto my-[2rem] w-[93rem]">
+            <div className="font-sans text-[3rem] mb-[2rem] mx-[3rem]">Team Projects</div>
+            <div className="container">
+                <a className="mx-[3rem]" href="">
+                    <div className="container-md w-[25rem] h-[15rem] inline-block shadow-[0rem_0rem_2rem_0.1rem_rgba(0,0,0,0.3)] rounded-[3rem] overflow-hidden">
+                        <img src="/atc-portfolio/logo512.png"/>
+                    </div>
+                </a>
+                <a className="mx-[3rem]" href="">
+                    <div className="container-md w-[25rem] h-[15rem] inline-block shadow-[0rem_0rem_2rem_0.1rem_rgba(0,0,0,0.3)] rounded-[3rem] overflow-hidden">
+                        <img src="/atc-portfolio/logo512.png"/>
+                    </div>
+                </a>
+                <a className="mx-[3rem]" href="">
+                    <div className="container-md w-[25rem] h-[15rem] inline-block shadow-[0rem_0rem_2rem_0.1rem_rgba(0,0,0,0.3)] rounded-[3rem] overflow-hidden">
+                        <img src="/atc-portfolio/logo512.png"/>
+                    </div>
+                </a>
+            </div>
+            <div className="font-sans text-[1.2rem] mt-[2rem] mx-[3rem]">
+                Text here
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
