6c9d83a04c4e8cbba851a75f954539eff8f1feab
Henry Gao <hgao080@aucklanduni.ac.nz>
2024-11-19 18:11:50 +1300

    fixed projects going off the screen on smaller screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,15 @@
 const Projects = () => {
     return (
-        <div className="flex flex-col w-[1200px] m-auto mt-[100px] pb-[150px]">
+        <div className="flex flex-col max-w-[1200px] m-auto mt-[100px] pb-[150px]">
             <h3 className="text-white font-header text-[40px]">TEAM PROJECTS</h3>
-            <div className="flex w-full gap-20 h-[200px] mt-[20px] mb-[30px]">
-                <div className="flex-1">
-                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-full h-full object-fill rounded-[2.25rem]" />
+            <div className="flex justify-center w-full min-h-[200px] mt-[20px] mb-[30px] flex-wrap gap-[73px]">
+                <div className="">
+                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-[350px] h-full object-fill rounded-[2.25rem]" />
                 </div>
-                <div className="flex-1">
-                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-full h-full object-fill rounded-[2.25rem]" />
-                </div><div className="flex-1">
-                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-full h-full object-fill rounded-[2.25rem]" />
+                <div className="">
+                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-[350px] h-full object-fill rounded-[2.25rem]" />
+                </div><div className="">
+                    <img src="/atc-portfolio/assets/imgPlaceholder.png" alt="" className="w-[350px] h-full object-fill rounded-[2.25rem]" />
                 </div>
             </div>
             <p className="text-white w-1/2 text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor ab et reiciendis ducimus cum eius, dolore aspernatur necessitatibus? Deleniti, necessitatibus!</p>
