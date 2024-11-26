e6a759d17354ae86423f6e63c379d4b72c7d5d2e
sli683 <sli683@aucklanduni.ac.nz>
2024-11-26 23:18:25 +1300

    home page with header, projects, developers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,13 @@ import Projects from "../components/Projects";
 
 const Home = () => {
     return (
-        <div>
+        <div className="bg-background bg-cover">
             <Header></Header>
             <Projects></Projects>
             <Developers></Developers>
+            <div className="h-[8rem] text-white mt-[10rem] pt-[3rem] text-center text-[1.2rem]">
+                Thank you for visiting our portfolio! We appreciate your time and support. Feel free to reach out to us for collaborations or inquiries
+            </div>
         </div>
     );
 }
